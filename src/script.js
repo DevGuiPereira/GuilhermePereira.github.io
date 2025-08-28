a841c3e2991a2b2708984300ba186debaaac35b6
Guilherme Reis Pereira <guilhermerp2004@gmail.com>
2025-08-27 23:02:47 -0300

    Update index.html and Sytle.css

--- src/script.js
+++ src/script.js
@@ -1,16 +1,22 @@
-// Espera o documento HTML ser completamente carregado antes de executar o script
 document.addEventListener("DOMContentLoaded", function () {
   // --- EFEITO DE DIGITAÇÃO COM TYPED.JS ---
-  // Apenas este código é necessário aqui. A rolagem é feita pelo CSS.
-
   const typingElement = document.querySelector("#typing-effect");
   if (typingElement) {
     const typed = new Typed("#typing-effect", {
-      strings: ["Analista de Dados.", "Entusiasta de Dados.", "Entusiasta de AI."],
+      strings: ["Analista de Dados", "Entusiasta de Dados", "Entusiasta de AI"],
       typeSpeed: 75,
       backSpeed: 50,
       backDelay: 1500,
       loop: true,
+    });
+  }
+
+  // --- MENU HAMBURGUER ---
+  const hamburger = document.querySelector(".hamburger-menu");
+  const nav = document.querySelector("nav");
+  if (hamburger && nav) {
+    hamburger.addEventListener("click", () => {
+      nav.classList.toggle("active");
     });
   }
 });
