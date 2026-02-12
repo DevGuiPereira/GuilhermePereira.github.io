673c16995dbc2c9f6c39d75534eb103b0b4fa8ca
Guilherme Reis Pereira <guilhermerp2004@gmail.com>
2026-02-12 15:16:08 -0300

    Add new images and update portfolio content

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const typingElement = document.querySelector("#typing-effect");
   if (typingElement) {
     const typed = new Typed("#typing-effect", {
-      strings: ["Analista de Dados", "Entusiasta de Dados", "Entusiasta de AI"],
+      strings: ["Cientista de Dados", "Entusiasta de AI"],
       typeSpeed: 75,
       backSpeed: 50,
       backDelay: 1500,
